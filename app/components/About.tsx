437ddf51273b876ffd2b74fab26f17b0e8562236
Lakshmanshankar <lakshmanshankarc@gmail.com>
2023-02-28 20:23:44 +0530

    done with skills component

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,33 +1,55 @@
-"use client"
-import React, { ReactHTMLElement, useEffect } from 'react'
-import { raleway, ubuntu } from '@/fonts/exports'
+"use client";
+import React, { ReactHTMLElement, useEffect } from "react";
+import { raleway, ubuntu } from "@/fonts/exports";
 function About() {
-
-    return (
-
-        <section className=' w-full h-max slow sm:pt-32 bg1 mt-0.5 pt-14' id='about' >
-            <h1 className={`${ubuntu.className} text1 background-change slow sm:text-9xl text-5xl sm:p-10`}>About Me</h1>
-            <p className={`${raleway.className} text-lg sm:text-3xl text2 p-5 sm:p10 sm:flex sm:full justify-center sm:items-center sm:px-40  sm:h-full`}>
-                I am a Javascript developer (👨‍🎓)  working on the NextJS fullstack applications and Docker products.
-                <br className='sm:none sm:hidden' /> <br />
-
-                As a computer science student, I have spent the last few years learning and developing my skills in various areas of computer science, such as programming languages, operating systems and devOps. I have had the opportunity to work on a range of projects, both individually and as part of a team, which has given me valuable experience in building and deploying applications.
-
-                <br className='sm:none sm:hidden' /><br className='sm:none sm:hidden' />
-                I am someone who is passionate about building innovative and exciting software applications. I believe that the key to creating truly remarkable software is staying up-to-date with the latest technologies and continuously expanding my skill set.
-                <br /><br />
-                As a linux user, I am part of a community that values flexibility, technical proficiency, and collaboration. Using Linux has helped me develop important skills  and has given me the opportunity to contribute to open-source projects and collaborate with others in the tech industry.
-
-                <br className='sm:none sm:hidden' /><br className='sm:none sm:hidden' />
-                Thank you for taking the time to read my About page. If you have any questions or would like to discuss potential projects, please feel free to contact me.
-            </p>
-            <div className={`${raleway.className} text-lg bg5 text1 flex justify-center `}>
-                created by lakshman with 💪
-            </div>
-        </section>
-    )
+  return (
+    <section
+      className=" w-full h-max slow sm:pt-32 bg1 mt-0.5 pt-14"
+      id="about"
+    >
+      <h1
+        className={`${ubuntu.className} text1 background-change slow sm:text-9xl text-5xl sm:p-10`}
+      >
+        About Me
+      </h1>
+      <p
+        className={`${raleway.className} text-lg sm:text-3xl text2 p-5 sm:p10 sm:flex sm:full justify-center sm:items-center sm:px-40  sm:h-full`}
+      >
+        I am a Javascript developer (👨‍🎓) working on the NextJS fullstack
+        applications and Docker products.
+        <br className="sm:none sm:hidden" /> <br />
+        As a computer science student, I have spent the last few years learning
+        and developing my skills in various areas of computer science, such as
+        programming languages, operating systems and devOps. I have had the
+        opportunity to work on a range of projects, both individually and as
+        part of a team, which has given me valuable experience in building and
+        deploying applications.
+        <br className="sm:none sm:hidden" />
+        <br className="sm:none sm:hidden" />
+        I am someone who is passionate about building innovative and exciting
+        software applications. I believe that the key to creating truly
+        remarkable software is staying up-to-date with the latest technologies
+        and continuously expanding my skill set.
+        <br />
+        <br />
+        As a linux user, I am part of a community that values flexibility,
+        technical proficiency, and collaboration. Using Linux has helped me
+        develop important skills and has given me the opportunity to contribute
+        to open-source projects and collaborate with others in the tech
+        industry.
+        <br className="sm:none sm:hidden" />
+        <br className="sm:none sm:hidden" />
+        Thank you for taking the time to read my About page. If you have any
+        questions or would like to discuss potential projects, please feel free
+        to contact me.
+      </p>
+      <div
+        className={`${raleway.className} text-lg bg5 text1 flex sm:text-3xl justify-center `}
+      >
+        created by lakshman with 💪
+      </div>
+    </section>
+  );
 }
 
-export default About
-
-
+export default About;
--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,65 +1,96 @@
-"use client"
-import React, { ReactNode, useEffect, useState } from 'react'
-import { ubuntu, raleway } from '@/fonts/exports'
-import Image from 'next/image'
+"use client";
+import React, { ReactNode, useEffect, useState } from "react";
+import { ubuntu, raleway } from "@/fonts/exports";
+import Image from "next/image";
 
 function Skills() {
-    return (
-        <section className=' bg1 mt-0.5 h-max py-10 sm:h-max w-full' id='projects'>
-            <h1 className={`${ubuntu.className} text3 slow sm:text-9xl text-5xl sm:p-10 text-clip`}>Skills</h1>
+  return (
+    <section className=" bg1 mt-0.5 h-max py-10 sm:h-max w-full" id="projects">
+      <h1
+        className={`${ubuntu.className} text3 slow sm:text-9xl text-5xl sm:p-10 text-clip`}
+      >
+        Skills
+      </h1>
 
-            <div className="grid-layout sm:p-5 shadowme">
-                <div className="three gitems sm:row-span-4">
-                    <div className="flex justify-between w-full bg4 ">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur temporibus aliquid ullam explicabo impedit totam, aperiam voluptas quia fuga. Nesciunt dignissimos magni qui ab consequatur illo commodi facilis sunt dolore, in voluptates maxime eaque, eius provident! Repudiandae vitae sint facilis!
-                    </div>
-                </div>
-                <div className="three gitems grid sm:row-span-2 bg2 sm:col-span-2 shadowme">
-                    <div className="flex justify-evenly w-full ">
-                        <SkillsIcons total={5} name={"typescript"} size={120} />
-                        <SkillsIcons total={5} name={"nextjs"} size={120} />
-                        <SkillsIcons total={5} name={"nodejs"} size={90} />
-                        <SkillsIcons total={5} name={"python"} size={120} />
+      <div className="grid-layout sm:p-5 shadowme">
+        <div className="three gitems sm:row-span-4 bg2 shadowme">
+          <div
+            className={`${raleway.className} sm:text-2xl texl-lg sm:px-14 text1 p-7`}
+          >
+            As a Computer Science student with a passion for building useful
+            applications, I am eager to learn new technologies and tools to
+            bring my ideas to life. Specifically, I am interested in developing
+            full-stack web applications using Next.js, TypeScript, and MySQL. I
+            am also working on other NoSQL databases like Azure CosmosDB and
+            MongoDB. working with various frameworks helps me choose which
+            framework to use to complete the tasks efficiently
+          </div>
+        </div>
+        <div className="three gitems grid sm:row-span-2 bg2 sm:col-span-2 shadowme">
+          <div className="flex justify-evenly w-full ">
+            <SkillsIcons total={5} name={"typescript"} size={120} />
+            <SkillsIcons total={5} name={"nextjs"} size={120} />
+            <SkillsIcons total={5} name={"nodejs"} size={90} />
+            <SkillsIcons total={5} name={"python"} size={120} />
+          </div>
+        </div>
+        <div className="three gitems sm:row-span-2 bg2 sm:col-span-2 h-full p-10 grid layout2">
+          <div className="devOps flex justify-evenly w-full ">
+            <SkillsIcons total={5} name={"ansible"} size={120} />
+            <SkillsIcons total={5} name={"docker"} size={90} />
+            <SkillsIcons total={5} name={"linux"} size={120} />
+            <SkillsIcons total={5} name={"azure"} size={120} />
+          </div>
+        </div>
 
-                    </div>
-                </div>
-                <div className="three gitems sm:row-span-2 bg2 sm:col-span-2 h-full p-10 grid layout2">
-                    <div className="devOps flex justify-evenly w-full ">
-                        <SkillsIcons total={5} name={"ansible"} size={120} />
-                        <SkillsIcons total={5} name={"docker"} size={90} />
-                        <SkillsIcons total={5} name={"linux"} size={120} />
-                        <SkillsIcons total={5} name={"azure"} size={120} />
-                    </div>
-                </div>
-
-                <div className="three gitems sm:row-span-2 bg2 justify-evenly sm:col-span-3 mt-2">
-                    <div className={`${raleway.className} sm:text-2xl texl-lg sm:px-14 text1 p-5`}>
-                        I follow the devOps principles to create CI/CD workflow for my projects, Docker to containerize my applications and deploy them on Azure cloud. Then I use Ansible to manage the configurations for the deployed applications.
-
-                        I am also proficient in using Azure CLI and other devOps tools such as Git version control system and Github Actions.
-                        This is my current stack of technologies that I use to build my projects.
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
+        <div className="three gitems sm:row-span-2 bg2 justify-evenly sm:col-span-3 mt-2">
+          <div
+            className={`${raleway.className} sm:text-2xl texl-lg sm:px-14 text1 p-5`}
+          >
+            As a Computer Science student with a keen interest in DevOps and
+            full-stack applications, I have been exploring Docker, Azure, and
+            Linux to expand my knowledge and skills. I am Using Docker to
+            containerize the applicationsc, create CI workflows for storing them
+            in diffrent container registries and use azure cloud to deploy them.
+            I am Mainly using Azure cosmosDB, Container Instances and Azure
+            functions. I am also proficient in using Azure CLI to create Azure
+            resources. This is the Technologies I am currently using to create
+            applications.
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Skills
+export default Skills;
 
+function SkillsIcons({
+  name,
+  size,
+  total,
+}: {
+  name: string;
+  size: number;
+  total: number;
+}): JSX.Element {
+  const [newsize, setNewsize] = useState(0);
+  useEffect(() => {
+    const element = document.getElementById(`${name}`);
+    // if (window) {
+    //     setNewsize(120)
+    // } else {
+    //     setNewsize(size)
+    // }
+  }, []);
 
-
-function SkillsIcons({ name, size, total }: { name: string, size: number, total: number }): JSX.Element {
-    const [newsize, setNewsize] = useState(0)
-    useEffect(() => {
-        const element = document.getElementById(`${name}`)
-        // if (window) {
-        //     setNewsize(120)
-        // } else {
-        //     setNewsize(size)
-        // }
-    }, [])
-
-    return <Image src={`/assets/${name}.svg`} width={newsize} height={newsize} alt='No Image' className=' p-1 sm:p-2 rounded-md inershad sm:w-32 backed bg-zinc-50 sm:h-32 w-16 h-16   shadow-lg'></Image>
-
+  return (
+    <img
+      src={`/assets/${name}.svg`}
+      width={newsize}
+      height={newsize}
+      alt="No Image"
+      className=" p-1 sm:p-2 rounded-md inershad sm:w-32 backed bg-zinc-50 sm:h-32 w-16 h-16 scale1  shadow-lg"
+    ></img>
+  );
 }

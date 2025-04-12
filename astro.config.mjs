e3e5d8e558805ff173d5cd0a065fdd1e51db2b9d
Lakshmanshankar <lakshmanshankarc@gmail.com>
2025-04-12 10:23:38 +0530

    feat: renamed and organized the code
    + added partytown for cf analytics
    ~ changed blog post structure
    ~ rename and organized code astro files
    + load font from file instead of google fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,23 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-    site:'https://lakshmanshankar.github.io',
-    integrations: [mdx(), sitemap({
-        entryLimit:10000,
-    }), tailwind()],
+    site: 'https://lakshmanshankar.github.io',
+    integrations: [
+        mdx(),
+        sitemap({
+            entryLimit: 10000,
+        }), 
+        tailwind(),
+        partytown({
+            // config:{
+            //     forward: ['cf-beacon'],
+            // }
+        }),
+    ],
     experimental: {
         responsiveImages: true,
         svg: true,

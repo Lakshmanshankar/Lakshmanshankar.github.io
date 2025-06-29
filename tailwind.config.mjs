695cd31b388acbd34eee255eca47eca8e645c409
Lakshmanshankar <lakshmanshankarc@gmail.com>
2025-06-29 12:32:36 +0530

    fix: fixed script rendering

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: ["class"],
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-      fontSize:{
-        base: '18px',
-      }
+    darkMode: ["class"],
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            animation: {
+                fade: "fadeIn 0.5s ease-out forwards",
+            },
+            keyframes: {
+                fadeIn: {
+                    "0%": { opacity: 0 },
+                    "50%": { scale: 1.1 },
+                    "100%": { opacity: 1 },
+                },
+            },
+            fontSize: {
+                sm: "16px",
+                base: "18px",
+            },
+        },
     },
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-	],
-}
+    plugins: [require("@tailwindcss/typography")],
+};

c2c9f85f888a5f98b247bb7fd74646a27ce18065
Lakshmanshankar <lakshmanshankarc@gmail.com>
2025-07-11 09:10:24 +0530

    created sitemap.xml manually (search console indexing issue)
    -
    
    -
    
    -

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-    // site: "https://lakshmanshankar.github.io",
-    site:"https://lakshmanshankar-github-io.vercel.app",
+    site: "https://lakshmanshankar.github.io",
     integrations: [
         mdx(),
-        sitemap({
-            entryLimit: 10000,
-            changefreq: "monthly",
-        }),
         tailwind(),
         partytown({
             // config:{

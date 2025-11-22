79c2a7612be5687e4521df3c6b26956c1688ac68
Lakshmanshankar <lakshmanshankarc@gmail.com>
2025-11-22 20:53:58 +0530

    added git rebase updateref blog

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,50 +2,50 @@ import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const blogSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    slug: z.string(),
-    date: z.coerce.date(),
-    draft: z.boolean().optional(),
-    tags: z.string().array().optional(),
-    image: z.string().optional(),
+  title: z.string(),
+  description: z.string(),
+  slug: z.string(),
+  date: z.coerce.date(),
+  draft: z.boolean().optional(),
+  tags: z.string().array().optional(),
+  image: z.string().optional(),
 });
 
 const projectSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    slug: z.string(),
-    date: z.coerce.date(),
-    demoURL: z.string().optional(),
-    repoURL: z.string().optional(),
-    draft: z.boolean().optional(),
+  title: z.string(),
+  description: z.string(),
+  slug: z.string(),
+  date: z.coerce.date(),
+  demoURL: z.string().optional(),
+  repoURL: z.string().optional(),
+  draft: z.boolean().optional(),
 });
 
 const workSchema = z.object({
-    company: z.string(),
-    role: z.string(),
-    dateStart: z.string(),
-    tech: z.string(),
-    description: z.string(),
-    dateEnd: z.string(),
-    draft: z.boolean().optional(),
+  company: z.string(),
+  role: z.string(),
+  dateStart: z.string(),
+  tech: z.string(),
+  description: z.string(),
+  dateEnd: z.string(),
+  draft: z.boolean().optional(),
 });
 
 const blog = defineCollection({
-    loader: glob({
-        pattern: ["**/*.md", "**/*.mdx"]
-        , base: "src/content/blog"
-    }),
-    schema: blogSchema,
+  loader: glob({
+    pattern: ["**/*.md", "**/*.mdx"],
+    base: "src/content/blog",
+  }),
+  schema: blogSchema,
 });
 const project = defineCollection({
-    loader: glob({ pattern: "**/**.md", base: "src/content/projects" }),
-    schema: projectSchema,
+  loader: glob({ pattern: "**/**.md", base: "src/content/projects" }),
+  schema: projectSchema,
 });
 
 const work = defineCollection({
-    loader: glob({ pattern: "**/**.md", base: "src/content/work" }),
-    schema: workSchema,
+  loader: glob({ pattern: "**/**.md", base: "src/content/work" }),
+  schema: workSchema,
 });
 
 export type BlogPost = z.infer<typeof blogSchema>;

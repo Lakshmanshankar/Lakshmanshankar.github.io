9d67cffe27e00a69896181c140ba4cdb7a9bfee5
Lakshmanshankar <lakshmanshankarc@gmail.com>
2026-06-07 16:42:50 +0530

    ui fixes
    Added a blog about intec 26

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,7 +32,7 @@ export default defineConfig({
     shikiConfig: {
       themes: {
         light: "vitesse-light",
-        dark: "vitesse-dark",
+        dark: "dark-plus",
       },
     },
   },
